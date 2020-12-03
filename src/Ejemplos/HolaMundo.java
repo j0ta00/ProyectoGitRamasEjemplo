3bb76005fc405300bb0b2c6515f3b49049ccc7ef
j0ta00 <74244181+j0ta00@users.noreply.github.com>
2020-12-03 14:22:46 +0100

    Update HolaMundo.java

--- src/Ejemplos/HolaMundo.java
+++ src/Ejemplos/HolaMundo.java
@@ -3,7 +3,7 @@ package Ejemplos;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("hello mundo");
+		System.out.println("Wola mundo");
 
 	}
 	public static void imprimeSaludo() { 
