24bbe84075283da3399fed722b5600ca1f3be410
jjmza <jjmza@DESKTOP-QCTN763.home>
2020-12-03 14:54:04 +0100

    Inicio

--- src/Ejemplos/HolaMundo.java
+++ src/Ejemplos/HolaMundo.java
@@ -3,7 +3,7 @@ package Ejemplos;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("otro mensaje");
+		System.out.println("Otro mensaje");
 	}
 	public static void imprimeSaludo() { 
 		System.out.println("Hei");	
